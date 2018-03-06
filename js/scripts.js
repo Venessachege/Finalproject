4b529a1e14ecb2222515372e31c7dccc783c15cb
venessachege <vchege.203@gmail.com>
2018-03-06 18:17:14 +0300

    Fixing bugs

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,8 @@ var config = {
   messagingSenderId: "999310279072"
 };
 firebase.initializeApp(config)
-// const preObject=document.getElementById('object')
-// //setting up realtime databaseURL
-// // Get a reference to the database service
-//
-// const ref=firebase.database().ref().child("object");
-// //sync object changes
-// ref.on("value" , snap => {
-//   preObject.innerText =JSON.stringify(snap.val() , null ,3)
-// });
-
 //database query
 var database = firebase.database();
-
 var uiConfig = {
   signInSuccessUrl: '/profile.html',
   signInOptions: [
@@ -34,11 +23,6 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 //start method will wait until the DOM is loaded
 ui.start("#firebaseui-auth-container", uiConfig);
 
-
-firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        console.log("logged in");
-
 firebase.auth().onAuthStateChanged(function(user){
 if (user) {
   console.log("logged in");
@@ -51,38 +35,7 @@ if (user) {
 
       });
     });
+}else{
 
-
-        $('#firebaseui-auth-container').hide()
-        $('li#member').hide();
-        $('a#signOut').show()
-
-//         user.providerData.forEach(function(profile) {
-//             var name = ("  Name: " + profile.displayName);
-//             var email = ("  Email: " + profile.email);
-//             var photo = ("  Photo URL: " + profile.photoURL);
-// });
-            $('a#signOut').click(function() {
-              firebase.auth().signOut().then(function() {
-                location.replace("/index.html");
-
-              })
-
-            });
-
-          } else {
-            console.log("not logged in");
-            $('a#signOut').hide();
-          }
-        });
-
-      $(document).ready(function() {
-        $("input#signout").click(function() {
-          firebase.auth().signOut().then(function() {
-            location.replace("/index.html");
-
-          })
-        });
-
-        $("ul#name").append(name);
-      });
+}
+});
