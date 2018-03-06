9dd026eab5881db9c29a35fdc38a7d329072a00d
Mbugua Munga <munga2mbugua@gmail.com>
2018-03-06 11:47:33 +0300

    creating user profile

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 //user interface logic
 
-// consttructor for the interface
+  // consttructor for the people to store profile
+
+ function person (picture,name,bestproject,personalPortfolio){
+      this.picture=picture;
+      this.name=name;
+      this.bestproject=bestproject;
+      this.personalPortfolio=personalPortfolio;
+ }
+
+ //array to store the people
+  var people=[];
+  // function to pus people into the array
+   var portPeople = function (){
+       //get all the requirements form person or database
+       var picture =       $('')
+   }
