617e902c9a9d8640e9eac145ceb2e97f54620c8b
muturi254 <petermuturi74@gmail.com>
2018-03-06 20:19:02 +0300

    user management

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,47 @@
 
 
-var config = {
-  apiKey: "AIzaSyDXxkIIamQTZQsTrrXhcJMuwfQEXE5P23A",
-  authDomain: "finalproject-6363d.firebaseapp.com",
-  databaseURL: "https://finalproject-6363d.firebaseio.com",
-  projectId: "finalproject-6363d",
-  storageBucket: "finalproject-6363d.appspot.com",
-  messagingSenderId: "999310279072"
-};
+// var config = {
+//   apiKey: "AIzaSyDXxkIIamQTZQsTrrXhcJMuwfQEXE5P23A",
+//   authDomain: "finalproject-6363d.firebaseapp.com",
+//   databaseURL: "https://finalproject-6363d.firebaseio.com",
+//   projectId: "finalproject-6363d",
+//   storageBucket: "finalproject-6363d.appspot.com",
+//   messagingSenderId: "999310279072"
+// }
+// ;
+ var config = {
+    apiKey: "AIzaSyByoAbvVGhI6Dsexa6bzx5A8JodfseqdCI",
+    authDomain: "helloworld-fd9ac.firebaseapp.com",
+    databaseURL: "https://helloworld-fd9ac.firebaseio.com",
+    projectId: "helloworld-fd9ac",
+    storageBucket: "helloworld-fd9ac.appspot.com",
+    messagingSenderId: "419845074355"
+  };
 firebase.initializeApp(config)
-//database query 
-var database = firebase.database(); 
+ // Get a reference to the database service
+// var database = firebase.database().ref('users');
+var user = firebase.auth().currentUser;
+
+if (user != null) {
+  user.providerData.forEach(function (profile) {
+    console.log("Sign-in provider: " + profile.providerId);
+    console.log("  Provider-specific UID: " + profile.uid);
+    console.log("  Name: " + profile.displayName);
+    console.log("  Email: " + profile.email);
+    console.log("  Photo URL: " + profile.photoURL);
+  });
+}
+
+
+
+
+
+
+
+
+
+
+//firebase ui
 var uiConfig = {
   signInSuccessUrl: '/profile.html',
   signInOptions: [
@@ -27,6 +58,18 @@ ui.start("#firebaseui-auth-container", uiConfig);
 
 firebase.auth().onAuthStateChanged(function(user){
 if (user) {
+  //loop to 
+
+  user.providerData.forEach(function (profile) {
+    console.log("Sign-in provider: " + profile.providerId);
+    console.log("  Provider-specific UID: " + profile.uid);
+    console.log("  Name: " + profile.displayName);
+    console.log("  Email: " + profile.email);
+    console.log("  Photo URL: " + profile.photoURL);
+    $('li#name').text(profile.displayName)
+    $('li#email').text(profile.email)
+  });
+  ///end loop
   console.log("logged in");
   $('#firebaseui-auth-container').hide();
   $('li#member').hide();
@@ -46,14 +89,13 @@ if (user) {
 
 
 
+// Database 
 
+
+
+ 
 
 
 $(document).ready(function() {
-  $("input#signout").click(function() {
-    firebase.auth().signOut().then(function() {
-      location.replace("/index.html");
-
-    })
-  });
+  
 });
