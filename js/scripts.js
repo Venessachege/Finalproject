9c59f93765f80ee960ab63464807ccba0cdc2703
muturi254 <petermuturi74@gmail.com>
2018-03-08 11:26:41 +0300

    synced scripts file up to date

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
-var config = {
-    apiKey: "AIzaSyByoAbvVGhI6Dsexa6bzx5A8JodfseqdCI",
-    authDomain: "helloworld-fd9ac.firebaseapp.com",
-    databaseURL: "https://helloworld-fd9ac.firebaseio.com",
-    projectId: "helloworld-fd9ac",
-    storageBucket: "helloworld-fd9ac.appspot.com",
-    messagingSenderId: "419845074355"
+ar config = {
+  apiKey: "AIzaSyDXxkIIamQTZQsTrrXhcJMuwfQEXE5P23A",
+  authDomain: "finalproject-6363d.firebaseapp.com",
+  databaseURL: "https://finalproject-6363d.firebaseio.com",
+  projectId: "finalproject-6363d",
+  storageBucket: "finalproject-6363d.appspot.com",
+  messagingSenderId: "999310279072"
 };
 
 
 firebase.initializeApp(config)
 // Get a reference to the database service
-var database = firebase.database().ref().child('user');
-var user = firebase.auth().currentUser;
 
-// if (user != null) {
-//   user.providerData.forEach(function(profile) {
-//     console.log("Sign-in provider: " + profile.providerId);
-//     console.log("  Provider-specific UID: " + profile.uid);
-//     console.log("  Name: " + profile.displayName);
-//     console.log("  Email: " + profile.email);
-//     console.log("  Photo URL: " + profile.photoURL);
-//   });
-// }
+
+// consttructor for the people to store profile
+
+// var Person = function(picture, name, id, email) {
+//   this.picture = picture;
+//   this.name = name;
+//   this.id = id;
+//   this.em = email;
+// };
+var userId = 0,
+  profile;
 //firebase ui
 
 var uiConfig = {
@@ -38,12 +38,12 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 //start method will wait until the DOM is loaded
 ui.start("#firebaseui-auth-container", uiConfig);
 
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
   if (user != null) {
     //loop to
 
 
-    user.providerData.forEach(function(profile) {
+    user.providerData.forEach(function (profile) {
       // console.log("Sign-in provider: " + profile.providerId);
       console.log("  Provider-specific UID: " + profile.uid);
       // profile = new Person(profile.photoURL,profile.displayName,profile.uid,profile.email)
@@ -63,8 +63,8 @@ firebase.auth().onAuthStateChanged(function(user) {
     $('#firebaseui-auth-container').hide();
     $('li#member').hide();
     $('a#signOut').show()
-    $('a#signOut').click(function() {
-      firebase.auth().signOut().then(function() {
+    $('a#signOut').click(function () {
+      firebase.auth().signOut().then(function () {
         location.replace("/index.html");
       });
     });
@@ -74,17 +74,17 @@ firebase.auth().onAuthStateChanged(function(user) {
 });
 // Database
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   var messagesRef = firebase.database().ref();
   var messageField = document.getElementById('messageInput');
   // var messageField = document.getElementById('messageInput2');
 
-  var messagePortfolio=document.getElementById("portfolio")
-  var messageTech=document.getElementById('tech')
-  var messageBestproject=document.getElementById('bestproject')
-  var messageHobbies=document.getElementById('hobbies')
-  var messageQuote=document.getElementById('quote')
+  var messagePortfolio = documrnt.getElementById("portfolio")
+  var messageTech = documrnt.getElementById('tech')
+  var messageBestproject = documrnt.getElementById('bestproject')
+  var messageHobbies = document.getElementById('hobbies')
+  var messageQuote = document.getElementById('quote')
   var messageResults = document.getElementById('results');
   // Save data to firebase
   function savedata() {
@@ -92,7 +92,7 @@ $(document).ready(function() {
 
     // messagesRef.child('users').child(userId).push(profile);
     // messageField.value = '';
-  // }
+    // }
     messagesRef.child('users').child(userId).push({
       fieldName: 'messageField',
       text: message
@@ -100,14 +100,14 @@ $(document).ready(function() {
     messageField.value = '';
   }
 
-  $("#savedata").click(function() {
+  $("#savedata").click(function () {
     savedata()
   })
 
 
 
-  setTimeout(function() {
-    messagesRef.child('users').child(userId).limitToLast(10).on('child_added', function(snapshot) {
+  setTimeout(function () {
+    messagesRef.child('users').child(userId).limitToLast(10).on('child_added', function (snapshot) {
       var data = snapshot.val();
       var message = data.text;
 
@@ -130,39 +130,39 @@ $(document).ready(function() {
 
 //array to store the peopl
 var people = [{
-    person1: {
-      name: "",
-      bestproject: "",
-      PersonalPortfolio: "",
-      Picturelink: "",
-    }
-  },
-  {
-    person2: {
-      name: "",
-      bestproject: "",
-      PersonalPortfolio: ""
-    }
-  },
-  {
-    person3: {
-      name: "",
-      bestproject: "",
-      PersonalPortfolio: ""
-    }
-  },
-  {
-    person4: {
-      name: "",
-      bestproject: "",
-      PersonalPortfolio: ""
-    }
-  },
-  {
-    person5: {
-      name: "",
-      bestproject: "",
-      PersonalPortfolio: ""
-    }
+  person1: {
+    name: "",
+    bestproject: "",
+    PersonalPortfolio: "",
+    Picturelink: "",
   }
+},
+{
+  person2: {
+    name: "",
+    bestproject: "",
+    PersonalPortfolio: ""
+  }
+},
+{
+  person3: {
+    name: "",
+    bestproject: "",
+    PersonalPortfolio: ""
+  }
+},
+{
+  person4: {
+    name: "",
+    bestproject: "",
+    PersonalPortfolio: ""
+  }
+},
+{
+  person5: {
+    name: "",
+    bestproject: "",
+    PersonalPortfolio: ""
+  }
+}
 ];
